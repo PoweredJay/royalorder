54eedfa107d43f864fc249e33163c38224dc10e0
PoweredJay <poweredjay@gmail.com>
2025-02-16 15:14:34 -0800

    Lol

--- Code/Menu/MenuSystem.cs
+++ Code/Menu/MenuSystem.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class MenuSystem : MonoBehaviour
 {
+    private AudioSource menuMusic;
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject keybindsMenu;
+    public GameObject audioMenu;
+
+    public Slider volumeSlider;
+    public Toggle musicToggle;
+    public Toggle sfxToggle;
+
+    public static float volume = 1;
+    public static bool musicOn = true;
+    public static bool sfxOn = true;
+    GameObject lastSelect;
     // Start is called before the first frame update
     void Start()
     {
