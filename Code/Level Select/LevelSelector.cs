39668f8c5fc5f66757806290872ffcd9711fe1a7
PoweredJay <poweredjay@gmail.com>
2025-02-17 19:54:15 -0800

    Menu music and selector screen

--- Code/Level Select/LevelSelector.cs
+++ Code/Level Select/LevelSelector.cs
@@ -5,8 +5,16 @@ using UnityEngine.EventSystems;
 
 public class LevelSelector : MonoBehaviour
 {
+    public MusicClass menuMusic;
+    public void Start()
+    {
+        menuMusic = GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>();
+        menuMusic.PlayMusic();
+        menuMusic.ChangeVolume(MenuSystem.globalVolume);
+    }
     public void LoadSelectedScene(string scene)
     {
         SceneManager.LoadScene(scene);
+        menuMusic.StopMusic();
     }
 }
