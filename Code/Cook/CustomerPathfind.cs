436977538e5cbb2d382e662f3fb9c33f5fe36d33
Lam Tran <lt84098@gmail.com>
2025-02-18 23:34:54 -0800

    customer move to open chairs

--- Code/Cook/CustomerPathfind.cs
+++ Code/Cook/CustomerPathfind.cs
@@ -5,7 +5,8 @@ public class CustomerPathfind : MonoBehaviour
 {
     private AIPath path;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private Transform target;
+    [SerializeField] public Transform target;
+    bool arrived;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,9 @@ public class CustomerPathfind : MonoBehaviour
     {
         path.maxSpeed = moveSpeed;
         path.destination = target.position;
+        if (!arrived && Vector2.Distance(this.GetComponent<Transform>().position, target.position) <= 0.015)
+        {
+            arrived = true;
+        }
     }
 }
