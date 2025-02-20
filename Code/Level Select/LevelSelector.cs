0de27f40057df4c15d35d9f0c870513e6dcb743a
PoweredJay <poweredjay@gmail.com>
2025-02-19 18:20:03 -0800

    Fixed music, title, level select screens

--- Code/Level Select/LevelSelector.cs
+++ Code/Level Select/LevelSelector.cs
@@ -15,6 +15,6 @@ public class LevelSelector : MonoBehaviour
     public void LoadSelectedScene(string scene)
     {
         SceneManager.LoadScene(scene);
-        menuMusic.StopMusic();
+        
     }
 }
